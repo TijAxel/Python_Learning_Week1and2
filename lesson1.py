ae6e82f4761db84fb5bdb1018e951e837105fb0f
Salazar <asalazar58@cps.k12.il.us>
2024-09-03 12:29:02 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,23 @@
+#this is a print statement
+#print statement allows us to print out a statement
+#comments are used to help us understand the code
+# print("hello my name is Axel Salazar")
+# print("I am a student at Jonh Hancock College Prep")
+# print("I am learning python")
+#create input for the user
+name=input("what is your name?")
+print("Hello", name)
+#ask the user for their age
+#print out their age
+#ask the user for their favorite color
+#print out their favorite color
+#ask the user for their facorite food
+#print out thier favorite food
+age=input("How old are you?")
+print("You are", age, "years old")
+color=input("what is your favorite color?")
+print("Your favorite color is" ,color, )
+food=input("what is your favorite food?")
+print("I've heard" ,food, "tastes really good")
+favorite_animal= input("What is your favorite animal?")
+print("Your favorite animal is a" + favorite_animal)
